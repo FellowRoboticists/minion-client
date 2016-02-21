3c99e2044bba0b43596e79c5fcdfd97e92a6dd5f
Dave Sieh <dj0hnve@gmail.com>
2016-02-21 09:38:51 -0700

    First light for minion 3.

--- config/robot.js
+++ config/robot.js
@@ -2,7 +2,34 @@ module.exports = (() => {
 
   var mod = {
 
-    name: 'sarah'
+    /**
+     * This is the name of your robot as it is known to the world.
+     * This name determines the names of the beanstalkd tubes that
+     * the client will communicate through.
+     */
+    name: 'sarah',
+
+    /**
+     * Here's how to find out what your serial port value 
+     * should be:
+     *
+     * After plugging in your Create USB cable you'll need to find 
+     * out what serial port your operating system has assigned your Create.
+     *
+     * On linux: make sure your user belongs to the 'dialout' group.
+     * 
+     * $ dmesg | grep tty
+     *
+     * On a mac:
+     * 
+     * $ ls /dev/tty.*
+     *
+     * On a windows box, open device manager and look under "Ports": 
+     * 
+     * C:\> mmc devmgmt.msc
+     *
+     */
+    serialport: '/dev/ttyUSB0'
 
   };
 
--- index.js
+++ index.js
@@ -8,76 +8,52 @@
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const queueSVC = require('./lib/queue-service');
+const signer = require('./lib/signer');
 const beanstalk = require('./config/beanstalk');
 const secrets = require('./config/secrets');
 const robotCFG = require('./config/robot');
+const RobotWorker = require('./lib/robot-worker');
+const iRobotCreate = require('create-oi');
 
-var __privateKey = null;
+//const respond = (robotName, command) => {
+  //var payload = {
+    //name: command
+  //};
 
-const __getPrivateKey = () => {
-  return new Promise( (resolve, reject) => {
-    if (!__privateKey) {
-      fs.readFile(secrets.robotKey, 'utf8', (err, key) => {
-        if (err) { return reject(err) }
-        __privateKey = key;
-        resolve(key);
-      });
-    } else {
-      resolve(__privateKey);
-    }
-  });
-};
+  //return __getPrivateKey().
+    //then( (key) => {
+      //var token = jwt.sign(payload, key, { algorithm: 'RS512' });
 
-const respond = (robotName, command) => {
-  var payload = {
-    name: command
-  };
+      //return queueSVC.queueJob('talker', robotName, 100, 0, 300, token);
+    //});
+//};
 
-  return __getPrivateKey().
-    then( (key) => {
-      var token = jwt.sign(payload, key, { algorithm: 'RS512' });
+iRobotCreate.init({ serialport: robotCFG.serialport });
 
-      return queueSVC.queueJob('talker', robotName, 100, 0, 300, token);
+iRobotCreate.on('ready', function() {
+
+  // Start up the beanstalk queuing
+  queueSVC.connect('incomingCommands', beanstalk.host, beanstalk.port).
+    then( () => {
+      fs.readFile(secrets.serverKey, 'utf8', (err, key) => {
+        if (err) { 
+          console.error("Error reading file: %j", err);
+          process.exit(1);
+        }
+        var worker = new RobotWorker(robotCFG.name, key, iRobotCreate);
+        console.log("Starting to listen on %sCommand", worker.robotName);
+        queueSVC.processRobotJobsInTube('incomingCommands', worker.robotName + 'Command', worker).
+          then( () => console.log("--") );
+        });
     });
-};
 
-const RobotWorker = function(robotName, publicKey) {
-
-  this.robotName = robotName;
-  this.publicKey = publicKey;
-
-  this.process = function(job) {
-    return new Promise( (resolve, reject) => {
-      var payload = jwt.verify(job.payload, this.publicKey, (err, payload) => {
-        if (err) { return reject(err) }
-        console.log("The payload: %j", payload);
-        respond(this.robotName, payload.command).
-          then( () => resolve() ).
-          catch( reject );
-      });
+  queueSVC.connect('talker', beanstalk.host, beanstalk.port).
+    then( () => {
+      // Tell the server we're ready to go
+      var token = signer.sign({ name: robotCFG.name, message: 'ready' });
+      queueSVC.queueJob('talker', robotCFG.name, 100, 0, 300, token);
     });
-  }
 
-};
+});
 
-
-
-// Start up the beanstalk queuing
-queueSVC.connect('incomingCommands', beanstalk.host, beanstalk.port).
-  then( () => {
-    fs.readFile(secrets.serverKey, 'utf8', (err, key) => {
-      if (err) { 
-        console.error("Error reading file: %j", err);
-        process.exit(1);
-      }
-      var worker = new RobotWorker(robotCFG.name, key);
-      console.log("Starting to listen on %sCommand", worker.robotName);
-      queueSVC.processRobotJobsInTube('incomingCommands', worker.robotName + 'Command', worker).
-        then( () => console.log("--") );
-      });
-  });
-
-queueSVC.connect('talker', beanstalk.host, beanstalk.port).
-  then( () => {
-    console.log("Talker ready for speaking");
-  });
+// TODO: Add other robot event here (like the bump handler)
