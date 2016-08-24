79166ad050b12da1e1c76819aa29de3b8bfd1df7
Naive Roboticist <naiveroboticist@gmail.com>
2016-08-24 17:13:51 -0600

    Restarting service and cleaning up releases.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -100,5 +100,9 @@ then
   queueCommand latestDir="\$(ls -t ${RELEASES_DIR} | tail -1)"
   queueCommand rm -f ${CURRENT_DIR}
   queueCommand cd ${BASE_DIR} \&\& ln -s releases/\${latestDir} current
+  queueCommand sudo systemctl restart minion
+  queueCommand numDirs=\$(ls ${RELEASES_DIR} | wc -l)
+  queueCommand if [ \$numDirs -gt 5 ]; then numDel=\$((numDirs-5)); cd ${RELEASES_DIR} && ls -t | tail -\${numDel} | xargs rm -f; fi
   invokeQueuedCommands ${MACHINE}
+  clearQueuedCommands
 fi
